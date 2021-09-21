d58260b23391ecb4fe11f88bd20f0a395ca7c655
olabijed@gmail.com <olabijed@gmail.com>
2021-09-21 14:50:14 +0100

    Math module created

---
+++ NetBot/Modules/MathModule.cs
@@ -0,0 +1,11 @@
+using Discord.Commands;
+
+namespace NetBot.Modules
+{
+    [Name("Math")]
+    [Summary("Math module for some math things")]
+    public class MathModule: ModuleBase<SocketCommandContext>
+    {
+        // TODO : eval function, convert function
+    }
+}
