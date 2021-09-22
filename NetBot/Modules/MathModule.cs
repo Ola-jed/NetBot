baaf062380494afdaf2d709030db405c5519934e
olabijed@gmail.com <olabijed@gmail.com>
2021-09-22 20:36:17 +0100

    Eval command

--- NetBot/Modules/MathModule.cs
+++ NetBot/Modules/MathModule.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Discord;
@@ -37,7 +39,13 @@ namespace NetBot.Modules
         [Summary("Evaluate a mathematical expression")]
         public async Task Eval(params string[] expression)
         {
-
+            var builder = new EmbedBuilder()
+            {
+                Color = Color.DarkMagenta
+            };
+            var result = new DataTable().Compute(string.Concat(expression), null);
+            builder.AddField("Result", result);
+            await ReplyAsync("", false, builder.Build());
         }
     }
 }
