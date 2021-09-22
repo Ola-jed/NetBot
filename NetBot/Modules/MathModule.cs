7ea1824835038d09b0edfc66402c565f5e50e5d9
olabijed@gmail.com <olabijed@gmail.com>
2021-09-22 18:01:03 +0100

    Math module update

--- NetBot/Modules/MathModule.cs
+++ NetBot/Modules/MathModule.cs
@@ -1,11 +1,43 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
 
 namespace NetBot.Modules
 {
     [Name("Math")]
     [Summary("Math module for some math things")]
-    public class MathModule: ModuleBase<SocketCommandContext>
+    public class MathModule : ModuleBase<SocketCommandContext>
     {
-        // TODO : eval function, convert function
+        [Command("sum")]
+        [Summary("Sum numbers of a sequence")]
+        public async Task Sum(params int[] args)
+        {
+            var builder = new EmbedBuilder()
+            {
+                Color = Color.Teal
+            };
+            builder.AddField("Result", args.Sum());
+            await ReplyAsync("", false, builder.Build());
+        }
+
+        [Command("mul")]
+        [Summary("Multiply the numbers of a sequence")]
+        public async Task Mul(params int[] args)
+        {
+            var builder = new EmbedBuilder()
+            {
+                Color = Color.DarkPurple
+            };
+            builder.AddField("Result", args.Aggregate((x, y) => x * y));
+            await ReplyAsync("", false, builder.Build());
+        }
+
+        [Command("eval")]
+        [Summary("Evaluate a mathematical expression")]
+        public async Task Eval(params string[] expression)
+        {
+
+        }
     }
 }
